b50e0c67abebd474f737bb85866013adc779d264
harouna-kanoute <44778626+harouna-kanoute@users.noreply.github.com>
2018-11-27 12:03:58 +0100

    js lesson 1 (#13)
    * exercise1
    
    * exercise2

---
+++ javascript/lesson1/exercise1/exercise/main.js
@@ -0,0 +1,12 @@
+var toto= "Bonjour"
+console.log (toto);
+toto=2+5;
+console.log (toto)
+toto=2-5;
+console.log (toto)
+toto=2*5;
+console.log(toto)
+toto = 2/5;
+console.log(toto)
+toto = 2%5;
+console.log(toto)
---
+++ javascript/lesson1/exercise2/exercise/main.js
@@ -0,0 +1,30 @@
+var nb1=2;
+var nb2=4;
+var nb3= nb1+nb2;
+console.log("Le résultat de :"+nb3+" + ")
+
+var nb1=2;
+var nb2=4;
+var nb3= nb1-nb2;
+console.log("Le résultat de :"+nb3+" + ")
+
+var nb1=2;
+var nb2=4;
+var nb3= nb1*nb2;
+console.log("Le résultat de :"+nb3+" + ")
+
+var nb1=2;
+var nb2=4;
+var nb3= nb1/nb2;
+console.log("Le résultat de :"+nb3+" + ")
+
+var nb1=2;
+var nb2=4;
+var nb3= nb1%nb2;
+console.log("Le résultat de :"+nb3+" + ")
+
+var x=2;
+var nb2=4;
+var nb3= x+nb2;
+console.log("Le résultat de :"+nb3+" + ")
+
